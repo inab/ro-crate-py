393f0f99bf96b54ebd14cbaf36eaac633b5d83fb
lrodrin <lrodrin@gmail.com>
2020-07-21 17:09:47 +0200

    fixed programming_language_entity not declared

--- rocrate/rocrate_api.py
+++ rocrate/rocrate_api.py
@@ -25,15 +25,15 @@ import rocrate.rocrate as roc
 from rocrate.model import entity
 from galaxy2cwl import get_cwl_interface
 
-def make_workflow_rocrate(workflow_path,wf_type,include_files=[],fetch_remote=False,cwl=None,diagram=None):
 
-    # Properties 
+def make_workflow_rocrate(workflow_path, wf_type, include_files=[], fetch_remote=False, cwl=None, diagram=None):
+    # Properties
     # missing? 
     # input
     # output
     # programmingLanguage
     # url
-    #version
+    # version
     # sdPublisher - current set to the person that provided the metadata, decision to change to the Workflow Hub itself - Done
 
     # publisher - where it came came from, e.g. Galaxy,  github, or WF Hub if uploaded - Done
@@ -55,28 +55,33 @@ def make_workflow_rocrate(workflow_path,wf_type,include_files=[],fetch_remote=Fa
 
     # Identifier - can be DOI if this function is enabled in WorkflowHub Done
 
-    #returns a complete ROCrate object corresponding to a Workflow template file
+    # returns a complete ROCrate object corresponding to a Workflow template file
     # wf_type: Galaxy, CWL , Nextflow..
     # cwl: CWL/CWL-Abstract representation of the workflow. If the
     # diagram: an image/graphical workflow representation.
     #         If a CWL/CWLAbstract file is provided then this is generated using cwltool
-    #abs_path = os.path.abspath(workflow_path)
+    # abs_path = os.path.abspath(workflow_path)
     wf_crate = roc.ROCrate()
     # add main workflow file
     file_name = os.path.basename(workflow_path)
-    wf_file = wf_crate.add_file(workflow_path,file_name)  # should I add it in a special path within the crate?
+    wf_file = wf_crate.add_file(workflow_path, file_name)  # should I add it in a special path within the crate?
     wf_crate.set_main_entity(wf_file)
-    #print(wf_file.id)
-    if wf_type != 'CWL':
-        if cwl:
-            programming_language_entity = entity.Entity(wf_crate,'https://www.commonwl.org/v1.1/', properties={"@type": ["ComputerLanguage", "SoftwareApplication"], 'name':'CWL', 'url':'https://www.commonwl.org/v1.1/', 'version':'1.1'})
-        elif wf_type == 'Galaxy':
-            #create cwl_abstract
-            with tempfile.NamedTemporaryFile(mode='w', delete=False) as cwl_abstract_out:
-                with redirect_stdout(cwl_abstract_out):
-                    get_cwl_interface.main(['1',workflow_path])
-            programming_language_entity = entity.Entity(wf_crate,'https://galaxyproject.org/')
-            wf_file = wf_crate.add_file(cwl_abstract_out.name, 'abstract_wf.cwl', properties={"@type": ["ComputerLanguage", "SoftwareApplication"]})
+
+    programming_language_entity = ""
+
+    if wf_type == 'CWL':
+        programming_language_entity = entity.Entity(wf_crate, 'https://www.commonwl.org/v1.1/', properties={
+            "@type": ["ComputerLanguage", "SoftwareApplication"], 'name': 'CWL',
+            'url': 'https://www.commonwl.org/v1.1/', 'version': '1.1'})
+
+    elif wf_type == 'Galaxy':
+        # create cwl_abstract
+        with tempfile.NamedTemporaryFile(mode='w', delete=False) as cwl_abstract_out:
+            with redirect_stdout(cwl_abstract_out):
+                get_cwl_interface.main(['1', workflow_path])
+        programming_language_entity = entity.Entity(wf_crate, 'https://galaxyproject.org/')
+        wf_file = wf_crate.add_file(cwl_abstract_out.name, 'abstract_wf.cwl',
+                                    properties={"@type": ["ComputerLanguage", "SoftwareApplication"]})
 
     ### SET PROPERTIES
     # A contextual entity representing a SoftwareApplication or ComputerLanguage MUST have a name, url and version,
@@ -93,7 +98,6 @@ def make_workflow_rocrate(workflow_path,wf_type,include_files=[],fetch_remote=Fa
         wf_type.append('SoftwareSourceCode')
     wf_file['@type'] = wf_type
 
-
     # if the source is a remote URL then add https://schema.org/codeRepository property to it
     # this can be checked by checking if the source is a URL instead of a local path
     if 'url' in wf_file.properties().keys():
@@ -102,6 +106,5 @@ def make_workflow_rocrate(workflow_path,wf_type,include_files=[],fetch_remote=Fa
     # add extra files    
     for file_entry in include_files:
         wf_crate.add_file(file_entry)
-        
-    return wf_crate
 
+    return wf_crate
