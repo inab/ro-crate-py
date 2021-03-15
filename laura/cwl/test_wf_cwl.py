fb2887d83317214b04a62473e21d777706d005ab
lrodrin <lrodrin@gmail.com>
2021-03-15 13:28:24 +0100

    deleted galaxy test

--- laura/cwl/test_wf_cwl.py
+++ laura/cwl/test_wf_cwl.py
@@ -1,25 +1,30 @@
-from rocrate import rocrate_api
+from rocrate import rocrate
+from rocrate.model import entity
+from rocrate.model.computationalworkflow import Workflow
+from pathlib import Path
+
+import ssl
 
 out_path = "/Users/laurarodrigueznavas/BSC/ro-crate-py/laura/cwl"
-out_path_with_filename = out_path + "/test_wf_cwl.zip"
+# out_path_with_filename = out_path + "/test_wf_cwl.zip"
 
-global wf, files_list
-
-option = int(input("Enter type workflow option:\n [1]: Basic \n"))
-
-if option == 1:
-    wf = "https://raw.githubusercontent.com/inab/vre_cwl_executor/master/tests/basic/data/workflows/basic_example.cwl"
-    files_list = [
-        '/Users/laurarodrigueznavas/BSC/vre_cwl_executor/tests/basic/NA12878.bam',
-        '/Users/laurarodrigueznavas/BSC/vre_cwl_executor/tests/basic/hg38.fa'
-    ]
+# change only for OSX
+ssl._create_default_https_context = ssl._create_unverified_context
 
 # Create RO-Crate
-crate = rocrate_api.make_workflow_rocrate(workflow_path=wf, wf_type="CWL", include_files=files_list)
+wf_crate = rocrate.ROCrate()
+wf_path = Path("https://github.com/inab/Wetlab2Variations/blob/eosc-life/cwl-workflows/workflows/workflow.cwl")
+
+print(wf_crate.root_dataset.as_jsonld())
+wf_file = Workflow(wf_crate, str(wf_path), wf_path.name)
+wf_crate._add_data_entity(wf_file)
+wf_crate.set_main_entity(wf_file)
+
+programming_language_entity = entity.Entity(wf_crate, 'https://www.commonwl.org/v1.1/',)
+wf_file['programmingLanguage'] = programming_language_entity
+
+inputs = entity.Entity(wf_crate)
+wf_file['inputs'].append("file1")
 
 # Write RO-Crate
-crate.write_crate(out_path)
-
-# Compress RO-Crate
-# crate.write_zip(out_path)
-# crate.write_zip(out_path_with_filename)
+wf_crate.write_crate(out_path)
--- laura/nextflow/test_wf_nf.py
+++ laura/nextflow/test_wf_nf.py
@@ -1,7 +1,7 @@
 from rocrate.rocrate import ROCrate
 
 wf = "/Users/laurarodrigueznavas/BSC/ro-crate-py/laura/nextflow/data/nextflow.nf"
-files_list = "/Users/laurarodrigueznavas/BSC/ro-crate-py/laura/nextflow/data/test_file_nf.txt"
+files_list = "/Users/laurarodrigueznavas/BSC/ro-crate-py/laura/nextflow/test_file_nf.txt"
 
 out_path = "/Users/laurarodrigueznavas/BSC/ro-crate-py/laura/nextflow"
 out_path_with_filename = out_path + "/test_wf_nf.zip"
@@ -13,7 +13,7 @@ lineList = [line.rstrip('\n') for line in open(files_list)]
 print(lineList)
 
 for file in lineList:
-    crate.add_file(file, fetch_remote = True)
+    crate.add_file(file)
 
 # Write RO-Crate
 crate.write_crate(out_path)
